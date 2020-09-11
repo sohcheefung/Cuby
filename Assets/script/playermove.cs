540b906d0da601d5d6f319304d169a2f261417f2
SOH CHEE FUNG <sohcheefung@gmail.com>
2020-09-11 08:16:57 +0800

    Update 1.3

--- Assets/script/playermove.cs
+++ Assets/script/playermove.cs
@@ -1,12 +1,15 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityStandardAssets.CrossPlatformInput;
 
 public class playermove : MonoBehaviour {
 	// reference to the rigidbody component 'r'
 	public Rigidbody r;
-
 	public float forwardForce = 2000f;
 	public float sidewaysForce = 500f;
+	
+	bool movingLeft;
+	bool movingRight;
 	
 	// marked this as 'fixedupdate' because we
 	// are using it to mess with physics
@@ -14,17 +17,39 @@ public class playermove : MonoBehaviour {
 	{
 		r.AddForce (0, 0, forwardForce * Time.deltaTime);
 
-		if(Input.GetKey("d"))
+		
+		if(movingLeft)
 		{
-			r.AddForce (sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);	
-		}
-		if(Input.GetKey("a"))
-		{
+			Debug.Log("Moving to left...");
 			r.AddForce (-sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);	
 		}
+		if(movingRight)
+		{
+			Debug.Log("Moving to right...");
+			r.AddForce (sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);	
+		}
+		
 		if(r.position.y < -1f)
 		{
 			FindObjectOfType<manager> ().endGame ();
 	}
 }
+
+	public void moveLeftOn(){
+		movingLeft = true;
+	}
+	
+	public void moveLeftOff(){
+		movingLeft = false;
+	}
+
+	public void moveRightOn(){
+		movingRight = true;
+	}
+	
+	public void moveRightOff(){
+		movingRight = false;
+	}
+
+	
 }
